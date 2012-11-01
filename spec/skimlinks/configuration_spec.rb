426f1cb21476b9694f508a050ba47ba9dbae7cd5
Manuel Meurer <manuel.meurer@gmail.com>
2012-11-01 07:28:15 +0100

    Check for Configuration::VALID_FOOS in spec

--- spec/skimlinks/configuration_spec.rb
+++ spec/skimlinks/configuration_spec.rb
@@ -8,10 +8,16 @@ describe Skimlinks::Configuration do
   describe '.configure' do
     Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
       it "should set the #{key}" do
-        Skimlinks.configure do |config|
-          config.send "#{key}=", key
+        valid_values_const_name = "VALID_#{key.to_s.pluralize.upcase}"
+        value = if Skimlinks::Configuration.const_defined?(valid_values_const_name)
+          Skimlinks::Configuration.const_get(valid_values_const_name).sample
+        else
+          Faker::Lorem.word
         end
-        Skimlinks.configuration.send(key).should eq(key)
+        Skimlinks.configure do |config|
+          config.send "#{key}=", value
+        end
+        Skimlinks.configuration.send(key).should eq(value)
       end
     end
   end
