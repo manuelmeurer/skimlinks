669a98127abfbefead56892f8fc76f0bbeb58837
Manuel Meurer <manuel.meurer@gmail.com>
2012-11-06 18:41:38 +0100

    Allow HTTP connections without VCR cassette in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'ffaker'
 
 VCR.configure do |config|
   config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.default_cassette_options = {
     record:            :new_episodes,
