e2485aeb75a365616cc47eec271cf4c64d974173
Manuel Meurer <manuel.meurer@gmail.com>
2012-11-06 18:41:08 +0100

    Add specs

--- spec/skimlinks/client_spec.rb
+++ spec/skimlinks/client_spec.rb
@@ -1,50 +1,78 @@
 require 'spec_helper'
 
 describe Skimlinks::Client do
-  describe 'with module configuration' do
-    [:module_config, :class_config].each do |config|
-      let(config) {
-        Hash[
-          *Skimlinks::Configuration::VALID_CONFIG_KEYS.map { |key| [key, valid_value_for_config(key)] }.flatten
-        ]
-      }
-    end
-
-    before do
-      Skimlinks.configure do |config|
-        Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
-          config.send "#{key}=", module_config[key]
-        end
+  context 'configuration' do
+    describe 'with module configuration' do
+      [:module_config, :class_config].each do |config|
+        let(config) {
+          Hash[
+            *Skimlinks::Configuration::VALID_CONFIG_KEYS.map { |key| [key, valid_value_for_config(key)] }.flatten
+          ]
+        }
       end
-    end
 
-    it 'should inherit module configuration' do
-      client = Skimlinks::Client.new
-
-      Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
-        client.send(key).should eq(module_config[key])
-      end
-    end
-
-    describe 'with class configuration' do
-      it 'should override module configuration' do
-        client = Skimlinks::Client.new(class_config)
-
-        Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
-          client.send(key).should eq(class_config[key])
+      before do
+        Skimlinks.configure do |config|
+          Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
+            config.send "#{key}=", module_config[key]
+          end
         end
       end
 
-      it 'should override module configuration after' do
+      it 'inherits module configuration' do
         client = Skimlinks::Client.new
 
-        class_config.each do |key, value|
-          client.send "#{key}=", value
+        Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
+          client.send(key).should eq(module_config[key])
+        end
+      end
+
+      describe 'with class configuration' do
+        it 'overrides module configuration' do
+          client = Skimlinks::Client.new(class_config)
+
+          Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
+            client.send(key).should eq(class_config[key])
+          end
         end
 
-        Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
-          client.send(key).should eq(class_config[key])
+        it 'overrides module configuration after' do
+          client = Skimlinks::Client.new
+
+          class_config.each do |key, value|
+            client.send "#{key}=", value
+          end
+
+          Skimlinks::Configuration::VALID_CONFIG_KEYS.each do |key|
+            client.send(key).should eq(class_config[key])
+          end
         end
+      end
+    end
+  end
+
+  context 'actions' do
+    use_vcr_cassette 'Skimlinks_Client'
+
+    before do
+      Skimlinks.configuration.api_key = 'foo'
+    end
+
+    describe '#merchant_categories' do
+      subject { Skimlinks::Client.new.merchant_categories }
+
+      it 'returns a non-empty hash' do
+        subject.should be_an_instance_of(Hash)
+        subject.should_not be_empty
+      end
+    end
+
+    describe '#merchant_category_ids' do
+      subject { Skimlinks::Client.new.merchant_category_ids }
+
+      it 'returns a non-empty array' do
+        subject.should be_an_instance_of(Array)
+        subject.should_not be_empty
       end
     end
   end
--- spec/skimlinks/merchant_search_spec.rb
+++ spec/skimlinks/merchant_search_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
 
 describe Skimlinks::MerchantSearch do
+  use_vcr_cassette
+
+  before do
+    Skimlinks.configuration.api_key = '85399dc089e6baf58e1d664670f4614e'
+  end
+
+  describe '#merchants_in_category' do
+    it 'returns an array' do
+      Skimlinks::MerchantSearch.new.merchants_in_category(1).should be_an_instance_of(Array)
+    end
+  end
+
+  describe '#merchants' do
+    it 'returns an array' do
+      Skimlinks::MerchantSearch.new.merchants.should be_an_instance_of(Array)
+    end
+  end
 end
